57a85a3bc3535a3a69d1ea360ee1f1c3113df1c6
peachgrim <peachgrim@gmail.com>
2022-11-20 15:42:03 +0100

    add: Card call to home

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,6 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { Button, View } from "react-native";
+import { Card } from "../card";
 export default function HomeView() {
   const { navigate } = useNavigation();
   const handleOnPress = () => {
@@ -8,6 +9,7 @@ export default function HomeView() {
   };
   return (
       <View>
+          <Card />
       </View> 
       );
 }
