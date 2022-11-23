7eea26d953f5b1da8e275620b8a64e9e958e6f85
peachgrim <peachgrim@gmail.com>
2022-11-23 21:31:25 +0100

    UPDATE : care

---
+++ app/models/pokemon.helpers.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+import { styles } from "../../components/styles/styles";
+import { IPokemon } from "../interfaces/Pokemon.interface";
+
+export const getHealthBar = (health: number) => {
+  const miam = [];
+  for (let i = 0; i < health; i++) {
+    miam.push("🍓");
+  }
+  return (
+    <View style={styles.divnourriture}>
+      <Text style={styles.health}>Niveau de faim :</Text>
+      <Text style={styles.divfaim}>{miam}</Text>
+    </View>
+  );
+};
+
+export const giveBackPokemon = (pokemon: IPokemon) => {};
